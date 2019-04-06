b07368e08ec9d9f1d711855741d0e1ddd4e72b81
Eduardo Rocha <edu181294@gmail.com>
2019-04-06 13:23:05 -0500

    successfully got data, implemented get cheapest quotes

--- browseQuotes.py
+++ browseQuotes.py
@@ -1,7 +1,7 @@
 ##########################################################################################
 # API used: https://rapidapi.com/skyscanner/api/skyscanner-flight-search?endpoint=5a9ca032e4b04378c0c99930
 #
-#    >>>>> Application for flight tickets search <<<<<
+#    >>>>> Get cheapest quotes from cache prices <<<<<
 #
 # note: Implemented for python 2 because of limitations of lybrary unirest
 # March 2019
@@ -31,18 +31,11 @@ required_params = {
   "originPlace": "SFO-sky",
   "destinationPlace": "LHR-sky",
   "outboundDate": "2019-05-01",
-  "adults": 1
 }
 
 # Optional parameters
 opt_params = {
   "inboundDate": "2019-05-10",
-  "cabinClass": "business",
-  "children": 0,
-  "infants": 0,
-  "includeCarriers": "",
-  "excludeCarriers": "",
-  "groupPricing": 0
 }
 
 # merge
@@ -52,43 +45,23 @@ parameters = merge_dicts(required_params, opt_params)
 ##########################################################################################
 # Search
 
-# POST Create session
+# GET Browse Quotes
 """
-Create a flight search session. A successful response contains no content. 
-The session key to poll the results is provided in the Location header of the 
-response. The last value of location header contains the session key which is 
-required when polling the session. 
+Retrieve the cheapest quotes from our cache prices.
 """
 
-source = "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/pricing/v1.0"
-post_response = unirest.post(source,
-  headers={
-    "X-RapidAPI-Key": "c5567760d2msh8aee5dc9ea49120p1b7f06jsndeadf5b02269",
-    "Content-Type": "application/x-www-form-urlencoded"
-  },
-  params = parameters
-)
+# GET source
+source = "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0"\
+  + "/" + required_params["country"]\
+  + "/" + required_params["currency"]\
+  + "/" + required_params["locale"]\
+  + "/" + required_params["originPlace"]\
+  + "/" + required_params["destinationPlace"]\
+  + "/" + required_params["outboundDate"]\
+  + "?inboundpartialdate=" + opt_params["inboundDate"]
+  
 
-# Add error handling here
-if False:
-  sys.exit()
-
-# GET Poll session results
-"""
-Get itineraries from a created session.
-Key received in post header must be inserted in src string.
-"""
-
-# session_key = ".../{session_key}""
-session_key = post_response.headers["Location"]
-last_slash_pos = session_key.rfind("/")
-session_key = session_key[last_slash_pos+1:]
-
-# insert session key in GET source
-source = "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/pricing/uk2/v1.0/"\
-  + session_key + "?"
-
-get_response = unirest.get(source,
+response = unirest.get(source,
   headers={
     "X-RapidAPI-Host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
     "X-RapidAPI-Key": "e08015148bmsh741474b8af3942ap1b29d7jsnc1343256972b"
@@ -98,6 +71,8 @@ get_response = unirest.get(source,
 # Add error handling here
 if False:
   sys.exit()
+
+print json.dumps(response.body, indent=4)
 
 ##########################################################################################
 # Handle data result
