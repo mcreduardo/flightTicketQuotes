7d3d2ae512a982e8832fc8b9222821005a70c2d6
Eduardo Rocha <edu181294@gmail.com>
2019-04-06 14:48:17 -0500

    implemented cheapest quotes browsing

--- browseQuotes.py
+++ browseQuotes.py
@@ -8,11 +8,9 @@
 # Eduardo Moura Cirilo Rocha
 ##########################################################################################
 
-
 import unirest
 import json
 import sys
-
 
 
 def merge_dicts(x, y):
@@ -20,69 +18,115 @@ def merge_dicts(x, y):
   z.update(y)
   return z
 
-##########################################################################################
-# Search Parameters
-
-# required parameters
-required_params = {
-  "country": "US",
-  "currency": "USD",
-  "locale": "en-US",
-  "originPlace": "SFO-sky",
-  "destinationPlace": "LHR-sky",
-  "outboundDate": "2019-05-01",
-}
-
-# Optional parameters
-opt_params = {
-  "inboundDate": "2019-05-10",
-}
-
-# merge
-parameters = merge_dicts(required_params, opt_params)
-
+def getCarrier(ids, carriers):
+  carriers_found = ""
+  for id in ids:
+    for carrier in carriers:
+      if carrier["CarrierId"] == id:
+        carriers_found = carriers_found + " " + carrier["Name"]
+  return carriers_found
 
 ##########################################################################################
-# Search
 
-# GET Browse Quotes
-"""
-Retrieve the cheapest quotes from our cache prices.
-"""
-
-# GET source
-source = "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0"\
-  + "/" + required_params["country"]\
-  + "/" + required_params["currency"]\
-  + "/" + required_params["locale"]\
-  + "/" + required_params["originPlace"]\
-  + "/" + required_params["destinationPlace"]\
-  + "/" + required_params["outboundDate"]\
-  + "?inboundpartialdate=" + opt_params["inboundDate"]
+def browseQuotes(required_params, printOutput):
   
+  # Search
+  # GET Browse Quotes
+  """
+  Retrieve the cheapest quotes from our cache prices.
+  """
+  # GET source
+  source = "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com"\
+    + "/apiservices/browsequotes/v1.0"\
+    + "/" + required_params["country"]\
+    + "/" + required_params["currency"]\
+    + "/" + required_params["locale"]\
+    + "/" + required_params["originPlace"]\
+    + "/" + required_params["destinationPlace"]\
+    + "/" + required_params["outboundDate"]\
+    + "/" + required_params["inboundDate"]
+  response = unirest.get(source,
+    headers={
+      "X-RapidAPI-Host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
+      "X-RapidAPI-Key": "e08015148bmsh741474b8af3942ap1b29d7jsnc1343256972b"
+    }
+  )
 
-response = unirest.get(source,
-  headers={
-    "X-RapidAPI-Host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
-    "X-RapidAPI-Key": "e08015148bmsh741474b8af3942ap1b29d7jsnc1343256972b"
-  }
-)
+  # Error handling 
+  if "ValidationErrors" in response.body:
+    print "\nValidation error when browsing quotes:\n"
+    print json.dumps(response.body["ValidationErrors"][0], indent=4)
+    print ""
+    sys.exit()
 
-# Add error handling here
-if False:
-  sys.exit()
+  #print json.dumps(response.body, indent=4)
+  #print response.headers
 
-print json.dumps(response.body, indent=4)
+
+
+
+
+  ##### Handle data result
+
+  # Search info
+  print ""
+  print "From " + response.body["Places"][1]["CityName"]\
+    + " (" + response.body["Places"][1]["SkyscannerCode"] + ")"\
+    + ", " + response.body["Places"][1]["CountryName"] + ", "\
+    + "to " + response.body["Places"][0]["CityName"]\
+    + " (" + response.body["Places"][0]["SkyscannerCode"] + ")"\
+    + ", " + response.body["Places"][0]["CountryName"] + "."
+  print "From " + required_params["outboundDate"]\
+    + " to " + required_params["inboundDate"] + "."
+
+  # Cheapest quotes:
+  print "\nCheapest quotes ("+ response.body["Currencies"][0]["Code"] +"):\n"
+
+  if printOutput:
+    i = 1
+    for quote in response.body["Quotes"]:
+      if quote["Direct"]: direct_str = ", direct"
+      else: direct_str = ", not direct"
+
+      print "\t".expandtabs(2) + str(i) + ") " + "MinPrice: "\
+        + str(quote["MinPrice"]) + direct_str
+      print "\tOutbound:".expandtabs(6)
+      print "\tDeparture: ".expandtabs(8) + quote["OutboundLeg"]["DepartureDate"]
+      print "\tCarrier(s): ".expandtabs(8)\
+        + getCarrier(quote["OutboundLeg"]["CarrierIds"], response.body["Carriers"])
+      print "\tInbound:".expandtabs(6)
+      print "\tDeparture: ".expandtabs(8) + quote["InboundLeg"]["DepartureDate"]
+      print "\tCarrier(s): ".expandtabs(8)\
+        + getCarrier(quote["InboundLeg"]["CarrierIds"], response.body["Carriers"])
+      print ""
+
+      i += 1
+  
+  return response
+
+
+
+
+
 
 ##########################################################################################
-# Handle data result
 
+if __name__=="__main__":
 
+  # Search Parameters
+  # required parameters
+  required_params = {
+    "country": "US",
+    "currency": "USD",
+    "locale": "en-US",
+    "originPlace": "SFO-sky",
+    "destinationPlace": "LHR-sky",
+    "outboundDate": "2019-05-01",
+    "inboundDate": "2019-05-10",
+  }
+  # Optional parameters
+  opt_params = {
+  }
 
-
-
-
-
-
-
+  browseQuotes(required_params = required_params, printOutput = True)
 
